ceb3476c4b462e821f97701753b0b326aa41fae3
Franco Campaiola <francocampaiola@hotmail.com>
2025-02-25 20:10:00 -0300

    refactor calculator logic; enhance operations and result calculations in useCalculator hook

--- 04-calculator-app/hooks/useCalculator.tsx
+++ 04-calculator-app/hooks/useCalculator.tsx
@@ -11,13 +11,21 @@ export const useCalculator = () => {
     const [formula, setFormula] = useState("0");
     const [number, setNumber] = useState("0");
     const [previousNumber, setPreviousNumber] = useState("0");
-
     const lastOperation = useRef<Operator>();
 
     useEffect(() => {
-        // TODO: Calcular subResultado
-        setFormula(number);
+        if (lastOperation.current) {
+            const firstFormulaPart = formula.split(" ").at(0)
+            setFormula(`${firstFormulaPart} ${lastOperation.current} ${number}`);
+        } else {
+            setFormula(number);
+        }
     }, [number]);
+
+    useEffect(() => {
+        const subResult = calculateSubResult();
+        setPreviousNumber(subResult.toString());
+    }, [formula]);
 
     const clean = () => {
         setNumber("0");
@@ -48,6 +56,69 @@ export const useCalculator = () => {
         } else {
             setNumber("0")
         }
+    }
+
+    const setLastNumber = () => {
+        // TODO: Calcular resultado
+        if (number.endsWith(".")) {
+            setPreviousNumber(number.slice(0, -1));
+        }
+
+        setPreviousNumber(number);
+        setNumber("0");
+    }
+
+    const divideOperation = () => {
+        setLastNumber()
+        lastOperation.current = Operator.divide;
+    }
+
+    const multiplyOperation = () => {
+        setLastNumber()
+        lastOperation.current = Operator.multiply;
+    }
+
+    const subtractOperation = () => {
+        setLastNumber()
+        lastOperation.current = Operator.subtract;
+    }
+
+    const addOperation = () => {
+        setLastNumber()
+        lastOperation.current = Operator.add;
+    }
+
+    const calculateSubResult = () => {
+        const [firstValue, operation, secondValue] = formula.split(" ");
+
+        const num1 = Number(firstValue)
+        const num2 = Number(secondValue)
+
+        if (isNaN(num2)) {
+            return num1
+        }
+
+        switch (operation) {
+            case Operator.add:
+                return num1 + num2
+            case Operator.subtract:
+                return num1 - num2
+            case Operator.multiply:
+                return num1 * num2
+            case Operator.divide:
+                return num1 / num2
+            default:
+                throw new Error("Invalid operation")
+        }
+
+
+    }
+
+    const calculateResult = () => {
+        const result = calculateSubResult();
+        setFormula(result.toString())
+        lastOperation.current = undefined;
+        setPreviousNumber("0");
     }
 
     const buildNumber = (numberString: string) => {
@@ -83,6 +154,12 @@ export const useCalculator = () => {
         buildNumber,
         clean,
         toggleSign,
-        deleteLast
+        deleteLast,
+        divideOperation,
+        multiplyOperation,
+        subtractOperation,
+        addOperation,
+        calculateSubResult,
+        calculateResult
     };
 };

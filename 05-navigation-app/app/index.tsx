69902bb136711c6918710cb8b26679fdb91d0d13
Franco Campaiola <francocampaiola@hotmail.com>
2025-02-28 15:54:24 -0300

    add custom fonts and global styles; implement layout and font loading in navigation app

--- 05-navigation-app/app/index.tsx
+++ 05-navigation-app/app/index.tsx
@@ -1,11 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const App = () => {
     return (
-        <View>
-            <Text>Hola mundo</Text>
-        </View>
+        <SafeAreaView>
+            <View className='mt-5 mx-2.5'>
+                <Text style={{ fontFamily: 'WorkSans-Black' }} className='text-4xl'>Hola mundo</Text>
+                <Text className='font-work-black text-3xl'>Hola mundo</Text>
+                <Text className='text-2xl font-work-medium'>Hola mundo</Text>
+                <Text className='text-1xl font-work-light'>Hola mundo</Text>
+            </View>
+        </SafeAreaView>
     )
 }
 

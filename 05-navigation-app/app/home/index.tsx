70542017741bf1c6441801993eeb5774a10b3f31
Franco Campaiola <francocampaiola@hotmail.com>
2025-03-05 16:20:43 -0300

    add CustomButton component; update HomeScreen to use CustomButton for navigation

--- 05-navigation-app/app/home/index.tsx
+++ 05-navigation-app/app/home/index.tsx
@@ -1,18 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Link } from 'expo-router'
+import React from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Link, router } from "expo-router";
+import CustomButton from "../../components/shared/CustomButton";
 
 const HomeScreen = () => {
     return (
         <SafeAreaView>
-            <View className='px-10'>
-                <Link className='mb-5' href={'/products'}>Productos</Link>
+            <View className="px-10">
+                <Link href='/products' asChild>
+                    <CustomButton className="mt-10 mb-10" color={'primary'}>Productos</CustomButton>
+                </Link>
+                <CustomButton className="mb-10" color="primary" onPress={() => router.push("/products")}>
+                    Productos
+                </CustomButton>
+                <CustomButton className="mb-10" color="primary" variant='text-only' onPress={() => router.push("/products")}>
+                    Productos
+                </CustomButton>
+                {/* <Link className='mb-5' href={'/products'}>Productos</Link>
                 <Link className='mb-5' href={'/profile'}>Perfil</Link>
-                <Link className='mb-5' href={'/settings'}>Ajustes</Link>
+                <Link className='mb-5' href={'/settings'}>Ajustes</Link> */}
             </View>
         </SafeAreaView>
-    )
-}
+    );
+};
 
-export default HomeScreen
+export default HomeScreen;

071b620786830dd6ba3561c779af8a8c772a6f08
Franco Campaiola <francocampaiola@hotmail.com>
2025-03-06 16:00:51 -0300

    add product details screen and product store; implement product listing with navigation

--- 05-navigation-app/app/(stack)/products/index.tsx
+++ 05-navigation-app/app/(stack)/products/index.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import { products } from "@/app/store/products.store";
+import { Link } from "expo-router";
 
 const ProductsScreen = () => {
     return (
         <View>
-            <Text>ProductsScreen</Text>
+            <FlatList
+                data={products}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                    <View className="mt-10 px-4">
+                        <Text className="text-2xl font-work-black">{item.title}</Text>
+                        <Text>{item.description}</Text>
+                        <View className="flex flex-row justify-between mt-2">
+                            <Text className="font-work-black">{item.price}</Text>
+                            <Link href={`/products/${item.id}`} className="text-primary">
+                                Ver detalles
+                            </Link>
+                        </View>
+                    </View>
+                )}
+            />
         </View>
-    )
-}
+    );
+};
 
-export default ProductsScreen
+export default ProductsScreen;

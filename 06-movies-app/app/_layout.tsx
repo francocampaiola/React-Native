e9ca057abeedf945882564a58a2d0034373d313a
Franco Campaiola <francocampaiola@hotmail.com>
2025-03-18 17:37:28 -0300

    initialize 06-movies-app with basic structure, assets, and configuration files

---
+++ 06-movies-app/app/_layout.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+import '../global.css'
+
+const RootLayout = () => {
+  return (
+    <View>
+      <Text className='text-6xl mx-auto pt-32'>RootLayout</Text>
+    </View>
+  )
+}
+
+export default RootLayout

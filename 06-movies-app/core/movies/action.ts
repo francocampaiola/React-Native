73ad4b0456ea6710d2bf8de653d3b6b6d6e7c6a1
Franco Campaiola <francocampaiola@hotmail.com>
2025-03-25 14:35:36 -0300

    remove console log from nowPlayingAction for cleaner output

--- 06-movies-app/core/movies/action.ts
+++ 06-movies-app/core/movies/action.ts
@@ -4,7 +4,7 @@ import { movieApi } from "../api/action";
 export const nowPlayingAction = async () => {
   try {
     const { data } = await movieApi.get<MovieDBMoviesResponse>("/now_playing");
-    console.log(JSON.stringify(data.results, null, 2));
+    // console.log(JSON.stringify(data.results, null, 2));
     return [];
   } catch (error) {
     console.log(error);
